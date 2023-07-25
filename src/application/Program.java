a29205e52c1b8a073a2cfd4ea25a645e9d89543d
Pedro Rosa <opedrorosa@hotmail.com>
2023-07-25 17:57:39 -0300

    Specials

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
@@ -48,10 +54,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
-		
 		UI.clearScreen();
 		UI.printMatch(chessMatch, captured);
-		
-		
 	}
 }
